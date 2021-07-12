27936741ba84805c7c540f26be83a366d83e797d
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-07-12 14:44:08 +0200

    Closure

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,6 @@
+function welcomeMessage(fullName) {
+    console.log("Welcome " + fullName);
+};
+const guillaume = welcomeMessage("Guillaume ");
+const alex = welcomeMessage("Alex");
+const fred = welcomeMessage("Fred");
