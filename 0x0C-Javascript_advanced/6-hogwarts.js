0cb83e5bb84ef995062162b0e5081d67f6f874ff
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-07-12 14:48:50 +0200

    Private methods with closure

---
+++ 0x0C-Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,41 @@
+class studentHogwarts {
+    constructor(name, privateScore) {
+    this.privateScore = 0;
+    this.name = null; 
+    };
+
+    changeScoreBy(points) {
+        this.privateScore += points;
+    };
+
+    setName(newName){
+        this.name = newName;
+    };
+    rewardStudent(){
+        this.changeScoreBy(1);
+    };
+    penalizeStudent(){
+        this.changeScoreBy(-1);
+    };
+    getScore(){
+        return (this.name + ": " + this.privateScore);
+    }
+
+}
+
+const harry = new studentHogwarts();
+harry.setName("Harry");
+for (i=0; i<4; i++)
+{
+    harry.rewardStudent()
+}
+console.log(harry.getScore())
+
+const draco = new studentHogwarts();
+draco.setName("Draco");
+draco.rewardStudent()
+for (i=0; i<3; i++)
+{
+    draco.penalizeStudent()
+}
+console.log(draco.getScore())
